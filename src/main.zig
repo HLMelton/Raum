d43a3ea236b9e2b7e31a4427ecedfe5ff42fa877
Hunter L. Melton <hunterlmelton@gmail.com>
2023-11-15 17:39:48 -0800

    basic server init

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,27 @@
 const std = @import("std");
+const print = std.debug.print; 
+const http = std.http;
+const log = std.log.scoped(.server);
+
+const serverAddress = "127.0.0.1";
+const serverPort = 8000;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var server = http.Server.init(allocator, .{ .reuse_address = true});
+    defer server.deinit();
 
-    try bw.flush(); // don't forget to flush!
-}
+    log.info("Server is running at {s}:{d}", .{serverAddress, serverPort});
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+
+    runServer(&server, allocator) catch |err|{
+        log.err("Server caught an error: {}\n", .{err});
+        if (@errorReturnTrace()) |trace|{
+            std.debug.dumpStackTrace(trace.*);
+        }
+        std.os.exit(1);
+    };
 }
